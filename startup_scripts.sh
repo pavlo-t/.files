2b2f2ebbd6beffb03f9f07a7d3de114ad0b0fe2b
Pavlo Tsapliienko <pavel.tsaplienko@gmail.com>
2022-08-07 01:35:48 +0300

    chown after adding /tmp/downoads/.directory

--- startup_scripts.sh
+++ startup_scripts.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 mkdir -p /tmp/downloads/.qBittorrent
-chown -R 1000:1000 /tmp/downloads
 echo "[Desktop Entry]
 Icon=folder-downloads" > /tmp/downloads/.directory
+chown -R 1000:1000 /tmp/downloads
